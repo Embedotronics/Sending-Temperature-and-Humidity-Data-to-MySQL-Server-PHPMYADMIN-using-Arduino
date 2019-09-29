4b4c0acd4b9a55013ad24c93bed35a7f93443511
Embedotronics <34275091+Embedotronics@users.noreply.github.com>
2019-09-29 17:48:27 +0530

    Update dht.php

--- dht.php
+++ dht.php
@@ -8,11 +8,11 @@ class dht11{
  
  function connect(){
   $this->link = mysqli_connect('localhost','root','') or die('Cannot connect to the DB');
-  mysqli_select_db($this->link,'temphumidnew') or die('Cannot select the DB');
+  mysqli_select_db($this->link,'<Your Database Name>') or die('Cannot select the DB');
  }
  
  function storeInDB($temperature, $humidity){
-  $query = "insert into dht11 set humidity='".$humidity."', temperature='".$temperature."'";
+  $query = "insert into <Your table name> set humidity='".$humidity."', temperature='".$temperature."'";
   $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
  }
  
